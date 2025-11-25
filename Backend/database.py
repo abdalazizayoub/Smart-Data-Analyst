82b851247b0e74360525a6adcc4d35607473e1c5
abdalazizayoub <abdulazizadil54@gmail.com>
2025-11-25 18:45:29 +0100

    fix database creation

--- Backend/database.py
+++ Backend/database.py
@@ -1,26 +1,22 @@
+# database.py
 import sqlite3
 
-conn = sqlite3.connect('smart_da.db',check_same_thread=False)
-cursor = conn.cursor()
-
-#create dataset if doesnot exist 
 def create_db():
+    conn = sqlite3.connect('smart_da.db')
+    cursor = conn.cursor()
     cursor.execute(""" 
-            CREATE TABLE IF NOT EXISTS Datasets (
+        CREATE TABLE IF NOT EXISTS Datasets (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             dataset_name TEXT,
             dataset_file TEXT,
             description TEXT,
             audio_path TEXT
-                   )""")
+        )""")
     conn.commit()
     conn.close()
-
-
+    print("Database table created successfully")
 
 def get_connection():
     conn = sqlite3.connect('smart_da.db')
     cursor = conn.cursor()
-    return conn , cursor
-
-
+    return conn, cursor
